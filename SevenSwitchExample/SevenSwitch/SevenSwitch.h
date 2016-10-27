32cf37bb7539cedb2400daf71e9c4cfe4225e55d
Artem Chabannyi <artem.chabanniy@gmail.com>
2016-10-27 11:42:17 +0300

    Added carthage support.

---
+++ SevenSwitchExample/SevenSwitch/SevenSwitch.h
@@ -0,0 +1,19 @@
+//
+//  SevenSwitch.h
+//  SevenSwitch
+//
+//  Created by Artem Chabannyi on 10/27/16.
+//  Copyright © 2016 Ben Vogelzang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SevenSwitch.
+FOUNDATION_EXPORT double SevenSwitchVersionNumber;
+
+//! Project version string for SevenSwitch.
+FOUNDATION_EXPORT const unsigned char SevenSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SevenSwitch/PublicHeader.h>
+
+
